fc50a789dc64874b3aef13ef39f0eff89a01c61e
Miroslav-Maltzev <125460844+Miroslav-Maltzev@users.noreply.github.com>
2023-04-04 16:11:18 +0300

    Solved exercise on exercism "booking up for beauty"

---
+++ booking-up-for-beauty/BookingUpForBeauty.cs
@@ -0,0 +1,31 @@
+using System;
+
+public static class Appointment
+{
+    public static DateTime Schedule(string appointmentDateDescription)
+    {
+        var isParse = DateTime.TryParse(appointmentDateDescription, out var result);
+        return result;
+    }
+
+    public static bool HasPassed(DateTime appointmentDate)
+    {
+        var currentDate = DateTime.Now;
+        return currentDate > appointmentDate;
+    }
+
+    public static bool IsAfternoonAppointment(DateTime appointmentDate)
+    {
+        return appointmentDate.Hour >= 12 && appointmentDate.Hour < 18;
+    }
+
+    public static string Description(DateTime appointmentDate)
+    {
+        return $"You have an appointment on {appointmentDate}.";
+    }
+
+    public static DateTime AnniversaryDate()
+    {
+        return new DateTime(DateTime.Now.Year, 9, 15);
+    }
+}
